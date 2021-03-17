652c98028adcbff285b4676b89c13f00424f06d8
Binh Nguyen <binh@MacBook-Pro.fritz.box>
2021-03-17 02:29:50 +0100

    little change

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./App.css";
+
+const App: React.VFC = () => {
+  return (
+    <div className="App">
+      <p>Hodensack</p>
+    </div>
+  );
+};
+
+export default App;
