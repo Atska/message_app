517ecb8b7903c7dac86214e556270220a6be3768
Binh <BinhNguyen93@web.de>
2021-03-28 20:05:38 +0200

    Added responsive card component. Login and sign up next

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@apollo/client";
 
 import NavBar from "./components/NavBar";
 import Card from "./components/Card";
+import "./App.css";
 
 //queries
 import { allPosts } from "./graphql/queries/allPosts";
@@ -10,10 +11,27 @@ import { allPosts } from "./graphql/queries/allPosts";
 const App: React.VFC = () => {
   const { loading, error, data } = useQuery(allPosts);
 
+  if (loading) return <p>Loading ...</p>;
+
   return (
     <div className="App">
       <NavBar />
-      <Card data={data} loading={loading} />
+      <div className="grid">
+        {data.allPosts.map((post: any) => {
+          return (
+            <Card
+              key={post.id}
+              username={post.username}
+              date={post.date}
+              text={post.text}
+              likes={post.likes}
+              comments={post.comments}
+              loading={loading}
+              error={error}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,62 +1,77 @@
 import * as React from "react";
-import "./Card.css";
-import logo from "../avatars/1.jpg";
-
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 //icons
 import { AiFillHeart, AiFillMessage } from "react-icons/ai";
-import { FaEdit, FaPenFancy } from "react-icons/fa";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 
-const filler =
-  "Im Haus war eine kleine Maus, die ging hinein und wieder hinaus. Der Käse war gut versteckt an einer sicheren Eck. Der Hunger trieb die Maus wild und sie suchte lange nach etwas zu essen. Oben am Fluss gibt es eine Wiese. Dort wachsen Blumen in allerlei Farben und Formen. Die einen sind blau und haben nur vier Blätter, andere jedoch sind bunt wie ein Regenbogen.";
-const Card = (props: { data: any; loading: boolean }) => {
-  const { data, loading } = props;
-  try {
-    console.log(data.allPosts[0]);
-  } catch (err) {
-    console.log(err);
-  }
+import "./Card.css";
+import logo from "../avatars/5.jpg";
+
+interface ICard {}
+
+const Card = (props: any) => {
+  const { error, loading } = props;
+
   if (loading) return <p>Loading ...</p>;
-  const { username, date, text, likes, comments } = data.allPosts[0];
+  if (error) return <p>404..</p>;
+
+  const { username, date, text, likes, comments } = props;
+
+  //format ISO-string time to relative time
+  dayjs.extend(relativeTime);
 
   return (
-    <div className="container-card">
-      <div className="container-header">
-        <div className="image-card">
-          <img src={logo} alt="avatar" className="profile-image"></img>
-        </div>
-        <div className="container-userinfo">
-          <p className="username">{username}</p>
-          <p className="date">{date}</p>
-        </div>
-      </div>
-      <div className="container-message">
-        <div className="message">
-          <p>{filler}</p>
-        </div>
-      </div>
-      <div className="container-btns">
-        <div className="action-btns">
-          <div className="like-btn">
-            <button>
-              <AiFillHeart />
-            </button>
+    <div className="container-post">
+      <div className="container-card">
+        <div className="container-header">
+          <div className="image-card">
+            <img src={logo} alt="avatar" className="profile-image"></img>
           </div>
-          <div className="comment-btn">
-            <button>
-              <AiFillMessage />
-            </button>
+          <div className="container-userinfo">
+            <h1 className="username">{username}</h1>
+            <p className="date">Posted {dayjs(date).fromNow()}</p>
           </div>
         </div>
-        <div className="post-btns">
-          <div className="update-btn">
-            <button>
-              <FaEdit />
-            </button>
+        <div className="container-wrap">
+          <div className="container-message">
+            <div className="message">
+              <p>
+                Hurensöhne alles hurensöhn diese miesen hurensöhne allese
+                verfickte missgeburten arschkfksfks dsknfsnvsjn fsjfsknfskfnsk
+                Pimmmmmmdsl sdjkdsnsnfsnuwf das ist das hjaus vidn das ist das
+                Haus vom Nicolaus der sankt martin kam aus frankreich und gab
+                seinen mantel einen penner der ein lied für ihn sang.
+              </p>
+            </div>
           </div>
-          <div className="post-btn">
-            <button>
-              <FaPenFancy />
-            </button>
+          <div className="container-btns">
+            <div className="action-btns flex">
+              <div className="like-btn flex">
+                <button className="flex-btn">
+                  {likes.length}
+                  <AiFillHeart />
+                </button>
+              </div>
+              <div className="comment-btn flex">
+                <button className="flex-btn">
+                  {comments.length}
+                  <AiFillMessage />
+                </button>
+              </div>
+            </div>
+            <div className="post-btns flex">
+              <div className="update-btn">
+                <button className="flex-btn">
+                  <FaEdit />
+                </button>
+              </div>
+              <div className="post-btn">
+                <button>
+                  <FaTrashAlt />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
